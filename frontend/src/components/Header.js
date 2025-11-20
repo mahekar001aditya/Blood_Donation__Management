e1f3e44d13670bc57230ee08ff52c6fa8e697ec1
Wax__01 <mahekaraditya468@gmail.com>
2025-11-20 18:22:31 +0530

    Create Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header style={{ background: "white", padding: "15px", color: "#B22222" }}>
+      <h2>🩸 BloodCare</h2>
+    </header>
+  );
+}
