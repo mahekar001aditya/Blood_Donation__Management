70c637d4633230be66683f08eb1bfb044e1a3985
Wax__01 <mahekaraditya468@gmail.com>
2025-11-20 18:27:48 +0530

    Update Patient.js

--- backend/ models/Patient.js
+++ backend/ models/Patient.js
@@ -1,1 +1,11 @@
 
+const mongoose = require("mongoose");
+
+const PatientSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  phone: String,
+  bloodType: String
+});
+
+module.exports = mongoose.model("Patient", PatientSchema);
