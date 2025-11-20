c3bb6337466f73671748d7f5aa92fda0bc3330d7
Wax__01 <mahekaraditya468@gmail.com>
2025-11-20 18:30:14 +0530

    Update Donors.js

--- frontend/src/components/Donors.js
+++ frontend/src/components/Donors.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
+
+const DonorSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  phone: String,
+  bloodType: String
+});
+
+module.exports = mongoose.model("Donor", DonorSchema);
 
