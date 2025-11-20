3ca2e033de5f7968aa8ff36ad1a8d6a79ea37824
Wax__01 <mahekaraditya468@gmail.com>
2025-11-20 18:26:31 +0530

    Update Hero.js

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,1 +1,11 @@
 
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="section">
+      <h1>Save Lives Through Blood Donation</h1>
+      <p>Your donation can save lives.</p>
+    </div>
+  );
+}
